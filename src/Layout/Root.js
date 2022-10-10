4f2b0c07764ff6a7aefae0aa97f24a6811353834
asifiqbalsagor <asifiqbalsagor98@gmail.com>
2022-10-11 00:24:19 +0600

    layout file added

--- src/Layout/Root.js
+++ src/Layout/Root.js
@@ -1,1 +1,17 @@
 
+import React from 'react';
+import Navbar from '../Navbar/Navbar';
+import Header from '../components/Header/Header'
+import { Outlet } from 'react-router-dom';
+
+const Root = () => {
+     return (
+          <div>
+               <Navbar></Navbar>
+               <Header></Header>
+               <Outlet></Outlet>
+          </div>
+     );
+};
+
+export default Root;
