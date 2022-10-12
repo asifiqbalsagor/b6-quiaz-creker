644ef962386b018f4b9cfcd7672bb8d9da1efe48
asifiqbalsagor <asifiqbalsagor98@gmail.com>
2022-10-12 20:45:20 +0600

    navbar some change

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -111,52 +111,32 @@ const Navbar = () => {
                     <ul class="space-y-4">
                       <li>
                         <NavLink
-                          to="/"
+                          to="/home"
                           aria-label="Our product"
                           title="Our product"
                           class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
                         >
-                          Product
+                          Home
                         </NavLink>
                       </li>
                       <li>
                         <NavLink
-                          to="/"
+                          to="/statistics"
                           aria-label="Our product"
                           title="Our product"
                           class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
                         >
-                          Features
+                          Statistics
                         </NavLink>
                       </li>
                       <li>
                         <NavLink
-                          to="/"
+                          to="/blog"
                           aria-label="Product pricing"
                           title="Product pricing"
                           class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
                         >
-                          Pricing
-                        </NavLink>
-                      </li>
-                      <li>
-                        <NavLink
-                          to="/"
-                          aria-label="About us"
-                          title="About us"
-                          class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
-                        >
-                          About us
-                        </NavLink>
-                      </li>
-                      <li>
-                        <NavLink
-                          to="/"
-                          class="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
-                          aria-label="Sign up"
-                          title="Sign up"
-                        >
-                          Sign up
+                          Blog
                         </NavLink>
                       </li>
                     </ul>
